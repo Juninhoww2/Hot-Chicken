6150e0bc11b1b697dc6151ccec857d72b97822b9
juninhoww2 <alqadianoKira@gmail.com>
2020-12-02 20:54:41 -0300

    Link do zap

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div>
             <h1>HOT-CHICKEN</h1>
-            <p>Whatsapp: <a href="facebook.com.br"><code>Whatsapp</code></a></p>
+            <p>Entre no grupo Hot-Chicken: <a href="https://www.youtube.com/redirect?q=https%3A%2F%2Fchat.whatsapp.com%2FEXvql86tiWW5Ja9B6NAtig&v=EVJourL_gg8&redir_token=QUFFLUhqblFlQnFBdGcwTWYtUzZhTnhpa2QyMmpLUlRaUXxBQ3Jtc0tsa1hFMnp1RERSWWJub1pjTktSVHhnZ0dwLS1lVFRBVENROXAwZnkwV21zS1o1aXdGVWZCa1hrcXV4cWlYTDZkbUhRYzJLUEN3eXZYX3pxa0RhakxsNHZrWUxFMnp1YlVsbjVlbUs2TXFVLUx0R2t6bw%3D%3D&event=video_description"><code>Entre no grupo Hot-Chicken:</code></a></p>
         </div>
     )  
 }

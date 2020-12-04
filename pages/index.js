70e0b60115ea39a951815e9dfb248d84ae0fdfbf
juninhoww2 <alqadianoKira@gmail.com>
2020-12-03 23:07:27 -0300

    Testando styles

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,19 @@ function Home() {
         <div>
             <h1>HOT-CHICKEN</h1>
             <p>Entre no grupo Hot-Chicken do ZapZap: <a href="https://chat.whatsapp.com/EXvql86tiWW5Ja9B6NAtig"><code>Hot-Chicken</code></a></p>
+            <style jsx>
+                {`
+                h1 {
+                    color: red;
+                }
+                p {
+                    color: black;
+                }
+                div: background: grey; {
+                }
+                `} </style>
         </div>
+
     )  
 }
 

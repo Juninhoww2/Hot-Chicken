a0bac7887647056a2910032a570e1e76b1cf2807
juninhoww2 <alqadianoKira@gmail.com>
2020-12-04 14:24:24 -0300

    import images to fotos

--- pages/fotos.js
+++ pages/fotos.js
@@ -1,10 +1,12 @@
-import votImage from './assets/vot.jpg'
+import votImage from './assets/vot.jpg';
 
 function Fotos() {
     return (<div>
             <h1>Fotos</h1>
             <a href="/">Home</a>
-            <img src={votImage} alt="" />
+            <header>
+                <img src={votImage} alt='vot' id='votImage' />
+            </header>
         </div>)
 }
 

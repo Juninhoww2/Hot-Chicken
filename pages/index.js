0a51803ba7e7dba13e71f0d64cc8ec1c10bdb41d
juninhoww2 <alqadianoKira@gmail.com>
2020-12-03 22:39:19 -0300

    Corrigindo variaveis

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-
+import { useState } from 'react'
+import Image from 'next/image'
 
 function Home() {
     return (
         <div>
             <h1>HOT-CHICKEN</h1>
-            <p>Entre no grupo Hot-Chicken: <a href="https://chat.whatsapp.com/EXvql86tiWW5Ja9B6NAtig"><code>Hot-Chicken</code></a></p>
+            <p>Entre no grupo Hot-Chicken do ZapZap: <a href="https://chat.whatsapp.com/EXvql86tiWW5Ja9B6NAtig"><code>Hot-Chicken</code></a></p>
         </div>
     )  
 }
